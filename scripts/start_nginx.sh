e6d78cbf82d9199771d20d29dad12d1fa1669895
vikrant <vikrant1013@gmail.com>
2019-08-29 20:19:03 +0530

    all files

--- scripts/start_nginx.sh
+++ scripts/start_nginx.sh
@@ -3,6 +3,11 @@
 
 chown emimall-prod:nginx -R /var/www/html
 
+cp -r /var/www/html/emimall-uat/app/code /var/www/html/app/
+cp -r /var/www/html/emimall-uat/app/design /var/www/html/app/
+
+
+
 sudo chmod -R 0777 generated/ pub/static/ var/*
 
 php bin/magento setup:upgrade
