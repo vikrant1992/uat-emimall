a89b7adc3017f7f5637de44ad8a20059447c2170
vikrant <vikrant1013@gmail.com>
2019-08-29 20:22:17 +0530

    all files

--- scripts/stop_nginx.sh
+++ scripts/stop_nginx.sh
@@ -4,6 +4,3 @@
 rm -rf /var/www/html/emimall-uat/app
 rm -rf /var/www/html/app/code
 rm -rf /var/www/html/app/design
-
-cp -r /var/www/html/emimall-uat/app/code /var/www/html/app/
-cp -r /var/www/html/emimall-uat/app/design /var/www/html/app/
