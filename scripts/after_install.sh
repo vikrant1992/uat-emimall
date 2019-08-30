5bed915d31feb980a0078d046d9019b4298741a9
vikrant <vikrant1013@gmail.com>
2019-08-30 10:16:17 +0530

    changed files names

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,3 +5,7 @@ chown emimall-prod:nginx -R /var/www/html
 
 cp -rf /var/www/html/emimall-uat/app/code /var/www/html/app/code
 cp -rf /var/www/html/emimall-uat/app/design /var/www/html/app/design
+
+php bin/magento setup:upgrade
+php bin/magento setup:static-content:deploy -f
+php bin/magento cache:flush
