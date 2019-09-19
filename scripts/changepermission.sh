4785462e483b689fd2e3f7085d5119a5ba678e65
vikrant1992 <vikrant1013@gmail.com>
2019-09-19 17:21:59 +0530

    Update changepermission.sh

--- scripts/changepermission.sh
+++ scripts/changepermission.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-chown emimall-uat:nginx -R /app/magento
-
+sudo sh /home/ec2-user/permission.sh
 find /app/magento -type d -exec chmod 755 {} \;
 find /app/magento -type f -exec chmod 755 {} \;
 
