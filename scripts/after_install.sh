39f5af6de60f5aa06511bf363317c83a75fadc30
vikrant1992 <vikrant1013@gmail.com>
2019-09-19 17:21:41 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # starting nginx service
 
-chown emimall-uat:nginx -R /app/magento
+sudo sh /home/ec2-user/permission.sh
 
 cp -rf /app/magento/emimall-uat/app/code /app/magento/app/code
 cp -rf /app/magento/emimall-uat/app/design /app/magento/app/design
