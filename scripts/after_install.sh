3900736faef6babe953fdaa2881fa78f1388151c
vikrant1992 <vikrant1013@gmail.com>
2019-09-19 17:28:20 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,6 +6,10 @@ sudo sh /home/ec2-user/permission.sh
 cp -rf /app/magento/emimall-uat/app/code /app/magento/app/code
 cp -rf /app/magento/emimall-uat/app/design /app/magento/app/design
 
+chmod -R 0777 /app/magento/var/
+chmod -R 0777 /app/magento/generated/
+chmod -R 0777 pub/static/
+
 php /app/magento/bin/magento setup:upgrade
 php /app/magento/bin/magento setup:static-content:deploy -f
 php /app/magento/bin/magento cache:flush
