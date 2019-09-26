400215220dbb154d3994181779fc3bb0dd9813e2
vikrant1992 <vikrant1013@gmail.com>
2019-09-26 13:31:47 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,15 +1,14 @@
+
 #!/bin/bash
 # starting nginx service
 
 sudo sh /home/ec2-user/permission.sh
 
-cp -rf /app/magento/emimall-uat/app/code /app/magento/app/code
-cp -rf /app/magento/emimall-uat/app/design /app/magento/app/design
+\cp -rf /app/magento/emimall-uat/app/code /app/magento/app
+\cp -rf /app/magento/emimall-uat/app/design /app/magento/app
 
-chmod -R 0777 /app/magento/var/
 chmod -R 0777 /app/magento/generated/
-chmod -R 0777 pub/static/
+chmod -R 0777 /app/magento/pub/static/
 
 php /app/magento/bin/magento setup:upgrade
 php /app/magento/bin/magento setup:static-content:deploy -f
-php /app/magento/bin/magento cache:flush
