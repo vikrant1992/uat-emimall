4fdcd1ea3582450353c8b91793c6919767ad2fd9
vikrant1992 <vikrant1013@gmail.com>
2019-09-26 13:32:22 +0530

    Update changepermission.sh

--- scripts/changepermission.sh
+++ scripts/changepermission.sh
@@ -3,5 +3,8 @@
 sudo sh /home/ec2-user/permission.sh
 find /app/magento -type d -exec chmod 755 {} \;
 find /app/magento -type f -exec chmod 755 {} \;
+chmod -R 0777 /app/magento/var/
+chmod -R 0777 /app/magento/generated/
+chmod -R 0777 /app/magento/pub/static/
 
-
+php /app/magento/bin/magento cache:flush
